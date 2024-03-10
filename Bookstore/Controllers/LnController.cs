f24b1e40104194c799974438559a4e17602dd57b
srinivasachary kandepi <srinivasacharykandepi@gmail.com>
2024-03-10 22:50:11 +0530

    Add project files.

---
+++ Bookstore/Controllers/LnController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class LnController : Controller
+    {
+        public IActionResult B2BFriend()
+        {
+            return View("chary");
+        }
+    }
+}
